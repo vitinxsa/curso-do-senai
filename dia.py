9cd3c0c7a089af4c586ae5f5c82baebc1e829b59
vitinxsa <vitorsodre484@gmail.com>
2025-05-15 15:53:31 -0300

    Add files via upload

---
+++ dia.py
@@ -0,0 +1,17 @@
+dia = input('digite o dia')
+if dia == '1':
+    print('domingo')
+if dia == '2':
+     print('segunda')
+if dia == '3':
+     print('terça')
+if dia == '4':
+     print('quarta_feira')
+if dia == '5':
+     print('quinta_feira')
+if dia == '6':
+     print('sexta feira')
+if dia == '7':
+     print('sabado')
+else:
+     print('ERROOR')
